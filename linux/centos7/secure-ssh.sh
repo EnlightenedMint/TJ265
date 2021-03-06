ab6bf02aed9774d9d25ecc426313e402a26e65ae
EnlightenedMint <behrx2@gmail.com>
2021-03-05 22:32:47 -0500

    added secure ssh script

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Bryan
+#Creates a new ssh user using #1 param
+#Adds a public key from the local repo or curled from the remote repo
+#remove roots ability to ssh in
+echo "Code goes here"
